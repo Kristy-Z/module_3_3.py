d8e03a0495e6aab4d2d1effc0c17566988322193
Kristy-Z <kristy38rus@gmail.com>
2024-09-06 18:34:20 +0700

    Update module_3_3.py

--- module_3_3.py
+++ module_3_3.py
@@ -8,18 +8,12 @@ print_params()
 print_params(a = 1, b = 25, c = True)
 print_params(a = 1, b = 25, c = [1,2,3])
 
-values_list = [False, 3, 'ok']
-values_dict = {'a':False, 'b':3, 'c':'ok'}
-
-def print_params(*values_list, **values_dict):
-    print(*values_list)
-    print(values_dict)
+values_list = [1, 'строка', True]
+values_dict = {'a':1, 'b':'строка', 'c':True}
 
 print_params(*values_list)
 print_params(**values_dict)
 
-values_list_2 = [54.32, 'Строка' ]
-print(*values_list_2)
-
+values_list_2 = [54.32, "'Строка'" ]
 print_params(*values_list_2, 42)
 
